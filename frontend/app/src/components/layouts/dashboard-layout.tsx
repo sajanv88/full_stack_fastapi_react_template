47003a8aff61e28293edec6eee6d560fb5fd6f1d
sajanv88 <work@sajankumarv.com>
2025-10-20 17:59:10 +0200

    chore: Fixed notification bar

--- frontend/app/src/components/layouts/dashboard-layout.tsx
+++ frontend/app/src/components/layouts/dashboard-layout.tsx
@@ -14,6 +14,7 @@ import { Badge } from "@/components/ui/badge";
 import { useEffect, useMemo } from "react";
 import { Loading } from "../shared/loading";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 
 
@@ -53,73 +54,72 @@ export function DashboardLayout() {
         )
     }
     return (
-        <div>
-            {!auth.user?.is_active && (
-                <div className="px-4 fixed z-50 w-full right-0 bg-secondary flex items-center justify-center">
-                    <p className="text-sm  p-0 pr-1 flex justify-center">
-                        Your account is not activated.
-                        Please check your email for the activation link. Or
-                    </p>
-                    <Button
-                        variant="link" size="sm" className="p-0 text-orange-300"
-                        onClick={auth.resendActivationEmail}
-                    >
-                        Resend Activation Email
-                    </Button>
-                </div>
-            )}
-            <SidebarProvider>
-                <DashboardSidebar />
-                <main className="sm:w-full">
+
+        <SidebarProvider>
+            <DashboardSidebar />
+            <main className="sm:w-full ">
+
+                <div className="flex justify-between">
                     <SidebarTrigger />
-                    <section className="w-full h-full flex flex-col">
-
-                        <div className="flex-1 flex-col">
-                            <header className="flex items-center p-2">
-                                <Avatar>
-                                    <AvatarImage src={userImage} />
-                                    <AvatarFallback>{auth.user?.first_name[0]}</AvatarFallback>
-                                </Avatar>
-                                <span className="ml-2 mr-auto flex-1 capitalize flex flex-col">
-                                    <span className="flex items-center w-[12rem] sm:w-[25rem]">
-                                        <span className="flex-1">
-                                            Welcome {auth.user?.last_name}
-                                        </span>
-
-                                        <span className="flex gap-2">
-                                            <Badge variant="secondary">Role: {auth.user?.role?.name}</Badge>
-                                            {isHost && (<Badge variant="secondary">Host Access</Badge>)}
-                                            {current_tenant && (<Badge variant="secondary">Tenant: {current_tenant?.name}</Badge>)}
-                                        </span>
-                                    </span>
-                                    <em className="text-xs text-muted-foreground mt-2">
-                                        {auth.user?.role?.name === "guest" && "You are currently a guest user. So, you can only view read-only content."}
-                                        {auth.user?.role?.name === "user" && "You are currently a regular user. So, you can view and edit your own content. Sometimes, edit others if you have permission."}
-                                        {auth.user?.role?.name === "admin" && "You are currently an admin user. So, you have full access to all resources."}
-                                    </em>
-
-                                </span>
-                                <DarkMode />
-                            </header>
-
-                            <section className="pt-10 flex-1">
-
-                                <div className="p-2">
-
-                                    <Outlet />
-                                    <Toaster />
-                                </div>
-
-                            </section>
-
+                    <DarkMode />
+                </div>
+                {!auth.user?.is_active && (
+                    <div className="relative w-full z-50 ">
+                        <div className="absolute w-full right-0 bg-secondary flex flex-col sm:flex-row items-center sm:justify-center">
+                            <p className="text-sm p-0 break-all w-[350px] text-center sm:w-auto">
+                                Your account is not activated.
+                                Please check your email for the activation link. Or
+                            </p>
+                            <Button
+                                variant="link" size="sm" className="p-0 pl-1 text-orange-300 w-fit"
+                                onClick={auth.resendActivationEmail}
+                            >
+                                Resend Activation Email
+                            </Button>
                         </div>
-                        <SimpleFooter />
-                    </section>
+                    </div>
+                )}
+                <section className="w-full h-full flex flex-col">
 
-                </main>
+                    <div className="flex-1 flex-col">
+                        <header className={cn("flex items-center p-2", {
+                            "mt-10": !auth.user?.is_active
+                        })}>
+                            <Avatar>
+                                <AvatarImage src={userImage} />
+                                <AvatarFallback>{auth.user?.first_name[0]}</AvatarFallback>
+                            </Avatar>
+                            <span className="pl-2 mr-auto flex-1 capitalize flex flex-col">
+                                <span className="flex items-center  sm:w-[25rem]">
+                                    <span className="flex-1">
+                                        Welcome {auth.user?.last_name}
+                                    </span>
 
-            </SidebarProvider>
-        </div>
+                                    <span className="flex gap-2">
+                                        <Badge variant="secondary">Role: {auth.user?.role?.name}</Badge>
+                                        {isHost && (<Badge variant="secondary">Host Access</Badge>)}
+                                        {current_tenant && (<Badge variant="secondary">Tenant: {current_tenant?.name}</Badge>)}
+                                    </span>
+                                </span>
+                                <em className="text-xs text-muted-foreground mt-2">
+                                    {auth.user?.role?.name === "guest" && "You are currently a guest user. So, you can only view read-only content."}
+                                    {auth.user?.role?.name === "user" && "You are currently a regular user. So, you can view and edit your own content. Sometimes, edit others if you have permission."}
+                                    {auth.user?.role?.name === "admin" && "You are currently an admin user. So, you have full access to all resources."}
+                                </em>
+
+                            </span>
+                        </header>
+                        <section className="pt-10 flex-1">
+                            <div className="p-2">
+                                <Outlet />
+                                <Toaster />
+                            </div>
+                        </section>
+                    </div>
+                    <SimpleFooter />
+                </section>
+            </main>
+        </SidebarProvider>
 
 
     )
