0250ca8661de40823b9c103baf9c462b96ea6272
sajanv88 <work@sajankumarv.com>
2025-10-25 08:12:01 +0200

    feat(#18): Stripe Billing system WIP

--- backend/api/infrastructure/externals/file_retrieval.py
+++ backend/api/infrastructure/externals/file_retrieval.py
@@ -1,10 +1,12 @@
-from api.common.exceptions import InvalidOperationException
+
+from api.common.utils import get_logger
 from api.core.exceptions import StorageNotEnabledException
 from api.domain.entities.storage_settings import StorageProvider, StorageSettings
 from api.infrastructure.externals.azure_storage import AzureBlobStorage
 from api.infrastructure.externals.s3_storage import S3Storage
+from api.core.config import settings
 
-
+logger = get_logger(__name__)
 class FileRetrieval:
     def __init__(self):
         from api.core.container import get_storage_settings_repository # To avoid circular imports
@@ -17,23 +19,35 @@ class FileRetrieval:
             raise StorageNotEnabledException if no provider is enabled
             or raise InvalidOperationException if the provider is unsupported.
         """
-        active_provider: StorageSettings = await self.storage_repository.single_or_none(is_enabled=True)
-        if active_provider is None:
-            raise StorageNotEnabledException()
+        try:
+            active_provider: StorageSettings = await self.storage_repository.single_or_none(is_enabled=True)
+            if active_provider is None:
+                raise StorageNotEnabledException()
 
-        if active_provider.provider.value == StorageProvider.AWS_S3.value:
+            if active_provider.provider.value == StorageProvider.AWS_S3.value:
+                s3 = S3Storage(
+                    bucket_name=active_provider.aws_bucket_name,
+                    aws_access_key=active_provider.aws_access_key,
+                    aws_secret_key=active_provider.aws_secret_key,
+                    region=active_provider.region
+                )
+                return await s3.generate_read_url(file_key, expires_in)
+            elif active_provider.provider.value == StorageProvider.AZURE_BLOB.value:
+                azure = AzureBlobStorage(
+                    connection_string=active_provider.azure_connection_string,
+                    container_name=active_provider.azure_container_name
+                )
+                return await azure.generate_read_url(file_key, expires_in)
+            else:
+               raise StorageNotEnabledException()
+            
+        except StorageNotEnabledException as se:
+
+            logger.warning(f"Unsupported storage provider: {str(se)}, Hence using default S3 storage")
             s3 = S3Storage(
-                bucket_name=active_provider.aws_bucket_name,
-                aws_access_key=active_provider.aws_access_key,
-                aws_secret_key=active_provider.aws_secret_key,
-                region=active_provider.region
+                bucket_name=settings.aws_s3_bucket_name,
+                aws_access_key=settings.aws_access_key_id,
+                aws_secret_key=settings.aws_secret_access_key,
+                region=settings.aws_region
             )
             return await s3.generate_read_url(file_key, expires_in)
-        elif active_provider.provider.value == StorageProvider.AZURE_BLOB.value:
-            azure = AzureBlobStorage(
-                connection_string=active_provider.azure_connection_string,
-                container_name=active_provider.azure_container_name
-            )
-            return await azure.generate_read_url(file_key, expires_in)
-        else:
-            raise InvalidOperationException(f"Unsupported storage provider: {active_provider.provider}")
--- backend/api/infrastructure/externals/file_upload.py
+++ backend/api/infrastructure/externals/file_upload.py
@@ -1,11 +1,11 @@
 from fastapi import UploadFile
 
-from api.common.exceptions import InvalidOperationException
 from api.common.utils import get_logger
 from api.core.exceptions import StorageNotEnabledException
 from api.domain.entities.storage_settings import StorageProvider, StorageSettings
 from api.infrastructure.externals.azure_storage import AzureBlobStorage
 from api.infrastructure.externals.s3_storage import S3Storage
+from api.core.config import settings
 
 logger = get_logger(__name__)
 
@@ -14,26 +14,38 @@ class FileUpload:
         from api.core.container import get_storage_settings_repository # To avoid circular imports
         self.storage_repository = get_storage_settings_repository()
 
+    
     async def upload_file(self, file: UploadFile) -> str:
         active_provider: StorageSettings = await self.storage_repository.single_or_none(is_enabled=True)
-        if active_provider is None:
-            raise StorageNotEnabledException()
+        try:
+            if active_provider is None:
+                raise StorageNotEnabledException()
 
-        if active_provider.provider.value == StorageProvider.AWS_S3.value:
-            logger.info(f"Uploading file to AWS S3: {file.filename}")   
+            if active_provider.provider.value == StorageProvider.AWS_S3.value:
+                logger.info(f"Uploading file to AWS S3: {file.filename}")   
+                s3 = S3Storage(
+                    bucket_name=active_provider.aws_bucket_name,
+                    aws_access_key=active_provider.aws_access_key,
+                    aws_secret_key=active_provider.aws_secret_key,
+                    region=active_provider.region
+                )
+                return await s3.upload_file(file, active_provider.aws_bucket_name + "/" + file.filename)
+            elif active_provider.provider.value == StorageProvider.AZURE_BLOB.value:
+                logger.info(f"Uploading file to Azure Blob Storage: {file.filename}")
+                azure = AzureBlobStorage(
+                    connection_string=active_provider.azure_connection_string,
+                    container_name=active_provider.azure_container_name
+                )
+                return await azure.upload_file(file, active_provider.azure_container_name + "/" + file.filename)
+            else:
+                raise StorageNotEnabledException()
+                
+        except StorageNotEnabledException as se:
+            logger.warning(f"Unsupported storage provider: {str(se)}, Hence using default S3 storage")
             s3 = S3Storage(
-                bucket_name=active_provider.aws_bucket_name,
-                aws_access_key=active_provider.aws_access_key,
-                aws_secret_key=active_provider.aws_secret_key,
-                region=active_provider.region
+                bucket_name=settings.aws_s3_bucket_name,
+                aws_access_key=settings.aws_access_key_id,
+                aws_secret_key=settings.aws_secret_access_key,
+                region=settings.aws_region
             )
-            return await s3.upload_file(file, active_provider.aws_bucket_name + "/" + file.filename)
-        elif active_provider.provider.value == StorageProvider.AZURE_BLOB.value:
-            logger.info(f"Uploading file to Azure Blob Storage: {file.filename}")
-            azure = AzureBlobStorage(
-                connection_string=active_provider.azure_connection_string,
-                container_name=active_provider.azure_container_name
-            )
-            return await azure.upload_file(file, active_provider.azure_container_name + "/" + file.filename)
-        else:
-            raise InvalidOperationException(f"Unsupported storage provider: {active_provider.provider.value}")
+            return await s3.upload_file(file, settings.aws_s3_bucket_name + "/" + file.filename)
